c72472af78c80c25e9c429aff45c7121cc9a718a
“Lukasz <lukasz.glowacz@glowacz.se>
2024-02-25 12:01:03 +0100

    added image filter choices and image validation method

---
+++ posts/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from .models import Profile
+
+class ProfileSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size larger than 2MB'
+            )
+        if value.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px'
+            )
+        if value.height > 4096:
+            raise serializers.ValidationError(
+                'Image height is latger than 4096px'
+            )
+        return value
+
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Profile
+        fields = [
+            'id', 'owner', 'is_owner', 'created_at', 'updated_at', 'name', 'content', 'image', 'image_filter'
+        ]
