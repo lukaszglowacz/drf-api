08a13fc642b09913e650d8f36ce014be8f678aef
“Lukasz <lukasz.glowacz@glowacz.se>
2024-02-26 13:14:05 +0100

    update post serializer and profile view to see followed_id, add likes_id to posts serializer

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from profiles import views
 
 urlpatterns = [
-    path('profiles/', views.ProflieList.as_view()),
-    path('profiles/<int:pk>/', views.ProfileView.as_view()),
+    path('profiles/', views.ProfileList.as_view()),
+    path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
 ]
