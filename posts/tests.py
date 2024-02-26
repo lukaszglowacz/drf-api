2929cfe8594a588d77d704523b025e1fc73a769a
“Lukasz <lukasz.glowacz@glowacz.se>
2024-02-26 11:07:10 +0100

    make tests for posts app, user can list the posts, logged in user can create a post, not logged in user cant create a post

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='lucka', password='test')
+
+    def test_can_list_posts(self):
+        lucka = User.objects.get(username='lucka')
+        Post.objects.create(owner=lucka, title='test title')
+        response = self.client.get('/posts/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+
+    def test_logged_in_user_can_create_post(self):
+        self.client.login(username='lucka', password='test')
+        response = self.client.post('/posts/', {'title': 'test title'})
+        count = Post.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_not_logged_in_user_cant_create_post(self):
+        response = self.client.post('/posts/', {'title': "test title"})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
